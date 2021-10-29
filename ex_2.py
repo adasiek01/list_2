6fb49ec36c25b0559e38b00e5ab6be90ed7621ee
Adam Kawałko <262329@student.pwr.edu.pl>
2021-10-29 23:06:42 +0200

    time

--- ex_2.py
+++ ex_2.py
@@ -1,13 +1,21 @@
+import time
+
+
 def pitagoras(x):
+    t_list = []
     x = (int(input("Enter the perimeter of a triangle: ")))
+    start = time.time()
     for a in range(1, x):
         for b in range(1, x):
             for c in range(1, x):
                 if a**2 + b**2 == c**2 and a + b + c == x:
-                    return a, b, c
+                    result = (a, b, c)
+                    t_list.append(result)
+    end = time.time()
+    if len(t_list) != 0:
+        return True, t_list[0], end - start
     else:
         return "There is no pythagorean triple whose sum is: " + str(x)
-
 
 def pitagoras_2(x):
     x = (int(input("Enter the perimeter of a triangle: ")))
@@ -47,4 +55,4 @@ def pitagoras_4(x):
 
 
 if __name__ == "__main__":
-    print(pitagoras_3(""))
+    print(pitagoras(""))
