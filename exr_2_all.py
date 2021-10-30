edb7def09edae51d4f8ab5d2ee4911bd49d313f3
Adam Kawałko <262329@student.pwr.edu.pl>
2021-10-30 21:54:14 +0200

    finished

--- exr_2_all.py
+++ exr_2_all.py
@@ -1,95 +1,103 @@
 import time
 import math
 
-def Pitagoras_v0(l):
+
+def pitagoras_v0(l):
+    """
+    Function that finds pythagorean triples
+    :param l: perimeter of a triangle
+    :return: pythagorean triple, time of work, number of operations
+    """
     counter = 0
     triangles = []
     start_time = time.time()
-    for a in range (1,l):
-        for b in range (1,l):
-            for c in range (1,l):
+    for a in range(1, l):
+        for b in range(1, l):
+            for c in range(1, l):
                 counter += 9
                 if a**2 + b**2 == c**2 and a+b+c == l:
-                    triangle = (a,b,c)
+                    triangle = (a, b, c)
                     triangles.append(triangle)
     end_time = time.time()
     if len(triangles) != 0:
-        return(True, triangles, end_time - start_time, counter)
+        return True, triangles, end_time - start_time, counter
     else: 
-        return(False, end_time - start_time, counter)
+        return False, end_time - start_time, counter
 
-def Pitagoras_v1(l):
+
+def pitagoras_v1(l):
     triangles = []
     start_time = time.time()
     triangle_condition = math.ceil(l/2)
     counter = 1
-    for a in range (1,triangle_condition):
-        for b in range (1,triangle_condition):
-            for c in range (1,triangle_condition):
+    for a in range(1, triangle_condition):
+        for b in range(1, triangle_condition):
+            for c in range(1, triangle_condition):
                 counter += 9
                 if a**2 + b**2 == c**2 and a+b+c == l:
-                    triangle = (a,b,c)
+                    triangle = (a, b, c)
                     triangles.append(triangle)
     end_time = time.time()
     if len(triangles) != 0:
-        return(True, triangles, end_time - start_time, counter)
+        return True, triangles, end_time - start_time, counter
     else: 
-        return(False, end_time - start_time, counter)
+        return False, end_time - start_time, counter
 
-def Pitagoras_v2(l):
+
+def pitagoras_v2(l):
     triangles = []
     start_time = time.time()
     triangle_condition = math.ceil(l/2)
     counter = 1
-    for a in range (1,triangle_condition):
-        for b in range (a,triangle_condition):
+    for a in range(1, triangle_condition):
+        for b in range(a, triangle_condition):
             counter += 7
             if a**2 + b**2 == (l-a-b)**2:
                 counter += 2
-                triangle = (a,b,(l-a-b))
+                triangle = (a, b, (l-a-b))
                 triangles.append(triangle)
     end_time = time.time()
     if len(triangles) != 0:
-        return(True, triangles, end_time - start_time, counter)
+        return True, triangles, end_time - start_time, counter
     else: 
-        return(False,end_time - start_time, counter)
+        return False, end_time - start_time, counter
 
 
-def Pitagoras_v3(l):
+def pitagoras_v3(l):
     triangles = []
     start_time = time.time()
     triangle_condition = math.ceil(l/2)
     counter = 1
-    for a in range (1,triangle_condition):
-        for b in range (a+1,triangle_condition):
-            counter += 6
-            if math.sqrt(a**2+b**2)%1 == 0:
+    for a in range(1, triangle_condition):
+        for b in range(a+1, triangle_condition):
+            counter += 7
+            if math.sqrt(a**2+b**2) % 1 == 0:
                 counter += 7
                 if a**2 + b**2 == (l-a-b)**2:
                     counter += 2
-                    triangle = (a,b,(l-a-b))
+                    triangle = (a, b, (l-a-b))
                     triangles.append(triangle)
     end_time = time.time()
     if len(triangles) != 0:
-        return(True, triangles, end_time - start_time, counter)
+        return True, triangles, end_time - start_time, counter
     else: 
-        return(False,end_time - start_time, counter)
+        return False, end_time - start_time, counter
 
 
-def Pitagoras_v4(l):
+def pitagoras_v4(l):
 
     triangles = []
     start_time = time.time()
     triangle_condition = math.ceil(l/2)
     max_of_short = int(l/3)
     counter = 2
-    for a in range (1,max_of_short):
+    for a in range(1, max_of_short):
         
-        for b in range (a+1,triangle_condition):
+        for b in range(a+1, triangle_condition):
 
             """
             a+1 -> 1
-            (a**2+b**2)%1 -> 4
+            (a**2+b**2) % 1 -> 4
             if ... == 0: -> 1
             a**2 + b**2 -> 3
             (l-a-b)**2 -> 3
@@ -98,52 +106,51 @@ def Pitagoras_v4(l):
             """
             counter += 6
             
-            if math.sqrt(a**2+b**2)%1 == 0:
+            if math.sqrt(a**2+b**2) % 1 == 0:
                 counter += 7
                 if a**2 + b**2 == (l-a-b)**2:
                     counter += 2
-                    triangle = (a,b,(l-a-b))
+                    triangle = (a, b, (l-a-b))
                     triangles.append(triangle)
         
     end_time = time.time()
     if len(triangles) != 0:
-        return(True, triangles, end_time - start_time, counter)
+        return True, triangles, end_time - start_time, counter
     else: 
-        return(False, end_time - start_time, counter)
+        return False, end_time - start_time, counter
 
 
-def Pitagoras_v4_sh(l):
+def pitagoras_v4_sh(l):
     triangles = []
     start_time = time.time()
     triangle_condition = math.ceil(l/2)
     max_of_short = int(l/3)
     counter = 2
-    k = 0
-    for a in range (1,max_of_short):
-        for b in range (a+1,triangle_condition):
+    for a in range(1, max_of_short):
+        for b in range(a+1, triangle_condition):
             square_sum = a**2+b**2
             counter += 6
-            if math.sqrt(square_sum)%1 == 0:
+            if math.sqrt(square_sum) % 1 == 0:
                 counter += 4
                 if square_sum == (l-a-b)**2:
                     counter += 2
-                    triangle = (a,b,(l-a-b))
+                    triangle = (a, b, (l-a-b))
                     triangles.append(triangle)
                      
     end_time = time.time()
     if len(triangles) != 0:
-        return(True, triangles, end_time - start_time, counter)
+        return True, triangles, end_time - start_time, counter
     else: 
-        return(False, end_time - start_time, counter)
+        return False, end_time - start_time, counter
 
 
-def Pitagoras_v5(l):
+def pitagoras_v5(l):
     triangles = []
     start_time = time.time()
     min_c = int(l/3)+1
     max_c = math.ceil(l/2)
     counter = 3
-    for c in range (min_c,max_c):
+    for c in range(min_c, max_c):
         a_b_square = c**2 - l**2 + 2*l*c
         counter += 7
         if a_b_square > 0:
@@ -153,16 +160,16 @@ def Pitagoras_v5(l):
                 counter += 5
                 b = int((l - c + a_b)/2)
                 a = int(l - c - b)
-                triangle = (a,b,c)
+                triangle = (a, b, c)
                 triangles.append(triangle)
     end_time = time.time()
     if len(triangles) != 0:
-        return(True, triangles, end_time - start_time, counter)
+        return True, triangles, end_time - start_time, counter
     else: 
-        return(False, end_time - start_time, counter)
+        return False, end_time - start_time, counter
 
 
-def Pitagoras_v5_short(l):
+def pitagoras_v5_short(l):
     start_time = time.time()
     check = 1
     min_c = int(l/3)+1
@@ -180,20 +187,20 @@ def Pitagoras_v5_short(l):
                 check = 1
                 counter += 5
                 end_time = time.time()
-                return(True, (a,b,c), end_time - start_time, counter)
+                return True, (a, b, c), end_time - start_time, counter
             else:
                 check = 0
     end_time = time.time()
     if check == 0:
-        return(False, end_time - start_time, counter)
+        return False, end_time - start_time, counter
     
 
 if __name__ == "__main__":
-    #print(Pitagoras_v0(30))
-    #print(Pitagoras_v1(30))
-    #print(Pitagoras_v2(30))
-    print(Pitagoras_v3(17002))
-    print(Pitagoras_v4(17002))
-    print(Pitagoras_v4_sh(17002))
-    print(Pitagoras_v5(170))
-    print(Pitagoras_v5_short(17002))
+    print(pitagoras_v0(12))
+    print(pitagoras_v1(12))
+    print(pitagoras_v2(12))
+    print(pitagoras_v3(12))
+    print(pitagoras_v4(12))
+    print(pitagoras_v4_sh(12))
+    print(pitagoras_v5(12))
+    print(pitagoras_v5_short(12))
